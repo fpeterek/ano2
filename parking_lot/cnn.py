5631245cc28a8304dac895e065be7d88fbcae76d
Filip Peterek <filip.peterek@firma.seznam.cz>
2022-11-12 21:51:22 +0100

    An even bigger model yields slightly better results

--- parking_lot/cnn.py
+++ parking_lot/cnn.py
@@ -13,11 +13,13 @@ import util
 class Net(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(1, 64, 3)
+        self.conv1 = nn.Conv2d(1, 64, 5)
         self.norm1 = nn.BatchNorm2d(64)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(64, 128, 3)
         self.norm2 = nn.BatchNorm2d(128)
+        self.conv3 = nn.Conv2d(128, 256, 3)
+        self.norm3 = nn.BatchNorm2d(256)
         self.fc1 = nn.LazyLinear(120)
         self.fc2 = nn.LazyLinear(84)
         self.fc3 = nn.LazyLinear(10)
@@ -26,6 +28,7 @@ class Net(nn.Module):
     def forward(self, x):
         x = self.pool(F.relu(self.norm1(self.conv1(x))))
         x = self.pool(F.relu(self.norm2(self.conv2(x))))
+        x = F.relu(self.norm3(self.conv3(x)))
         x = torch.flatten(x, 1)  # flatten all dimensions except batch
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
